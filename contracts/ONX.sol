0716a42548315271b2a4a5aa452b33b239328814
unknown <puja.pl14@gmail.com>
2021-07-30 00:38:47 +0530

    Merged with manaan/timelock

--- contracts/ONX.sol
+++ contracts/ONX.sol
@@ -12,26 +12,21 @@ contract ONX is ERC20,Ownable{
     mapping(string=>bool) public phase_tracker; // true if phase ended
     string[] public phases;
     uint public achived_supply_milestone;
-    uint public release_period = 1 seconds; //1 minutes; // 7 days;  // 7 days
+    uint public release_period = 7 days;  // 7 days
     uint public inflation_rate = 2;
-    //event log_str(string data);
-    //event log(uint len);
     uint public inflation_flag;
     uint public inflation_target_supply;
-    
     
     constructor() ERC20('ONX Token', 'ONX') Ownable() {
         achived_supply_milestone = 100000;
         _mint(msg.sender, achived_supply_milestone);
         latestTimestamp = block.timestamp;
     }
-        
-
+    
     function addSupplyPhase(string calldata phase, uint phase_cap) public onlyOwner() {
         require(max_supply[phase] == 0, "Phase already exists");
         max_supply[phase] = phase_cap;
-        phases.push(phase);
-        
+        phases.push(phase);  
     }
     
     function setInflationRate(uint _inflation_rate) public onlyOwner(){
@@ -39,15 +34,26 @@ contract ONX is ERC20,Ownable{
         require(_inflation_rate > 0, "Rate must be greater than zero");
         inflation_rate = _inflation_rate;
         inflation_target_supply = achived_supply_milestone + (achived_supply_milestone * _inflation_rate)/100;
-    
     }
     
-        
+    function addSupplyPhase(string calldata phase, uint phase_cap) public onlyOwner() {
+        require(max_supply[phase] == 0, "Phase already exists");
+        max_supply[phase] = phase_cap;
+        phases.push(phase);
+    }
+    
+    function setInflationRate(uint _inflation_rate) public onlyOwner(){
+        require(inflation_flag == 1, "Phase has no yet ended");
+        require(_inflation_rate > 0, "Rate must be greater than zero");
+        inflation_rate = _inflation_rate;
+        inflation_target_supply = achived_supply_milestone + (achived_supply_milestone * _inflation_rate)/100;
+    }
+    
     function  release_funds() public onlyOwner() isReleaseTime(){
         if(inflation_flag == 0){
             for(uint i=0; i<phases.length; i++){
                 if(phase_tracker[phases[i]] == false){
-                    uint amt_to_mint = (max_supply[phases[i]] - achived_supply_milestone)/2;
+                    uint amt_to_mint = (max_supply[phases[i]] - achived_supply_milestone)/50;
                     _mint(msg.sender, amt_to_mint);
                     latestTimestamp = block.timestamp;
                     if(totalSupply() >= max_supply[phases[i]]){
@@ -63,7 +69,7 @@ contract ONX is ERC20,Ownable{
                         release_inflation_funds();
                     }
                 }
-        }
+            }
         }
         else{
             release_inflation_funds();
@@ -71,14 +77,14 @@ contract ONX is ERC20,Ownable{
     }
     
     function  release_inflation_funds() internal {
-        uint amt_to_mint = (inflation_target_supply - achived_supply_milestone)/2;
+        uint amt_to_mint = (inflation_target_supply - achived_supply_milestone)/50;
         _mint(msg.sender, amt_to_mint);
         latestTimestamp = block.timestamp;
         if(totalSupply() >= inflation_target_supply){
             achived_supply_milestone = totalSupply();
             inflation_target_supply = achived_supply_milestone + (achived_supply_milestone * inflation_rate)/100;
         }
-}
+    }
 
     modifier isReleaseTime() {
         require(block.timestamp >= (latestTimestamp+ release_period), "Hold up dude!");
